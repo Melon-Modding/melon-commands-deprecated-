b88642eef2f8fbab47fd377cb430a5ba2035956a
David Flowers II <master4491@gmail.com>
2024-01-14 18:18:16 -0600

    No more internal server area. Works as expected.

--- src/main/java/prophetsama/testing/mixin/OverrideSpawnCommand.java
+++ src/main/java/prophetsama/testing/mixin/OverrideSpawnCommand.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = SpawnCommand.class, remap = false)
 public abstract class OverrideSpawnCommand {
 
-	@Inject(method = "opRequired", at = @At("RETURN"))
+	@Inject(method = "opRequired", at = @At("RETURN"), cancellable = true)
 	public void melon_opRequired(CallbackInfoReturnable<Boolean> cir) {
 		cir.setReturnValue(false);
     }
